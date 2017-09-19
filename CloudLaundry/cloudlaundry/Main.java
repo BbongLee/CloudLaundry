0d622a7d021ad72285488df682afa0df8c96c5e6
kimyunyeong0006 <iuki1@e-mirim.hs.kr>
2017-09-19 23:48:12 +0900

    Add files via upload

---
+++ CloudLaundry/cloudlaundry/Main.java
@@ -0,0 +1,13 @@
+package cloudlaundry;
+
+public class Main {
+	public static final int SCREEN_WIDTH = 1000;
+	public static final int SCREEN_HEIGHT = 700;
+
+	
+	public static void main(String[] args) {
+
+		new Laundry();
+
+	}
+}
