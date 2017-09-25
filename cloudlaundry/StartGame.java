014ccb0bffd574edc6b042e1fb85d728b9427888
ProGlobe22 <jebon0722@gmail.com>
2017-09-25 18:50:27 +0900

    20170925_ JLabel구름 넣기
    진행중!

--- cloudlaundry/StartGame.java
+++ cloudlaundry/StartGame.java
@@ -22,31 +22,26 @@ class StartGame extends JFrame{
 	//private Image background = new ImageIcon(Main.class.getResource("../image/GameBG.png")).getImage();
 	private JPanel leftJP; //구름있는 패널
 	private JPanel rightJP; //설명(?)있는 패널
-	private JLabel[] cloudsJL = new JLabel[20];
+	private JLabel cloudsJL[] = new JLabel[20];
 	private ImageIcon cloudClean = new ImageIcon(Main.class.getResource("../image/cloudClean.png")); 
 	private ImageIcon cloudDirtyBasic = new ImageIcon(Main.class.getResource("../image/cloudDirtyBasic.png")); 
 	private ImageIcon cloudDirtyEntered = new ImageIcon(Main.class.getResource("../image/cloudDirtyEntered.png"));
 	
-	private ImageIcon cloudBubble1 = new ImageIcon(Main.class.getResource("../image/cloudBubble_01.png"));
+	private ImageIcon cloudBubble1 = new ImageIcon((Main.class.getResource("../image/cloudBubble_01.png")));
 	private ImageIcon cloudBubble2 = new ImageIcon(Main.class.getResource("../image/cloudBubble_02.png"));
 	private ImageIcon cloudBubble3 = new ImageIcon(Main.class.getResource("../image/cloudBubble_03.png"));
 	private ImageIcon cloudBubble4 = new ImageIcon(Main.class.getResource("../image/cloudBubble_04.png"));
 	//구름 오염도 
-	java.net.URL file1 = getClass().getResource("../image/CloudClean1.png");
-	java.net.URL file2 = getClass().getResource("../image/CloudClean2.png");
-	java.net.URL file3 = getClass().getResource("../image/CloudClean3.png");
-	java.net.URL file4 = getClass().getResource("../image/CloudClean4.png");
-	java.net.URL file5 = getClass().getResource("../image/CloudClean5.png");
 	private ImageIcon[] cloudDirty = new ImageIcon[5];
-	cloudDirty[0] = file1;
-	cloudDirty[1] = file2;
-	cloudDirty[2] = file3;
-	cloudDirty[3] = file4;
-	cloudDirty[4] = file5;
 	
 	//cloudsJL[i] = new JLabel(cloudDirty[image])); 
 	//칸 사이즈 가로 : 160, 세로 : 125px
 	public StartGame (Laundry laundry){
+
+		for(JLabel label : cloudsJL ) {
+			label = new JLabel();
+		}
+		
 		frame = laundry;
 		setUndecorated(true);  //기본 메뉴바 안보임
 		setTitle("구름세탁중 . . .");
@@ -57,6 +52,7 @@ class StartGame extends JFrame{
 		setVisible(true); // 화면에 보이게 해줌
 		setBackground(new Color(0, 0, 0, 0));  //배경은 하얀색
 		setLayout(null);
+		
 		//left JPanel
 		//cloudDirty
 		for(int i=0; i<5; i++) {
@@ -67,15 +63,9 @@ class StartGame extends JFrame{
 		//for(int i=0;i<20;i++) {
 		//	cloudsJL[i].setBounds(25,25,160*(i/4),125*(i/5));
 		//}
-			//	랜덤으로 들어갈 개수만큼 이미지배열방(ex)20개중 5개) 구하기.
-		for(int i=0; i<20; i++) {
-		//랜덤으로 더러움정도 넣기!, 정도에따라 스페이스바 횟수가 달라짐.
-			cloudsJL[i] = new JLabel(cloudDirty[(int)(Math.random()*5)]);
-			System.out.println(cloudsJL[i]);
-		}
-		//좌표생성 규칙 : 수정해야함
-		int x;
-		for(int i=0; i<20; i++) {
+		//좌표생성 규칙 
+		int x = 0;
+		for(int i=0; i<cloudsJL.length; i++) {
 			switch((int)(i%4)) {
 			case 0:case 4:case 8:case 12:case 16:
 				x=0;
@@ -93,9 +83,18 @@ class StartGame extends JFrame{
 				System.out.println("좌표생성 에러!");
 				break;
 			}
-			cloudsJL[i].setBounds(25+x*160,25+(i/4)*125,160,125);
+			cloudsJL[i].setBounds(25+x*160, 25+(i/4)*125, 160, 125);
 		}
-		leftJP.add(cloudsJL[1]); 
+		//	랜덤으로 들어갈 개수만큼 이미지배열방(ex)20개중 5개) 구하기.
+		for(int i=0; i<cloudsJL.length; i++) {
+		//랜덤으로 더러움정도 넣기!, 정도에따라 스페이스바 횟수가 달라짐.
+			cloudsJL[i] = new JLabel(cloudDirty[(int)(Math.random()*5)]);
+			//System.out.println(cloudsJL[i]);
+		}
+		
+		for (int i=0; i<cloudsJL.length; i++) {
+			leftJP.add(cloudsJL[i]); 
+		}
 		leftJP.setBounds(25,25,640,650);
 		leftJP.setLayout(null);
 
