8111654f07723f642ee3602973a8f398ac458f04
ProGlobe22 <jebon0722@gmail.com>
2017-10-23 07:45:45 +0900

    1020-1022 / 구름 클래스 & 객체  생성

--- GameStart.java
+++ GameStart.java
@@ -1,4 +1,4 @@
-package exam;
+package cloudlaundry_test;
 
 import java.awt.Color;
 import java.awt.Cursor;
@@ -8,7 +8,6 @@ import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -18,166 +17,197 @@ import javax.swing.JPanel;
 
 import cloudlaundry.Main;
 
-class GameThread extends Thread {
-	public void run() {
-
+class GameObject {
+	public int[] level = {3, 5, 7}; //내려올 구름 갯수!
+	int lev = 0; //레벨 0 - 1 - 2 setLev로 바꿀 예정입니당!
+	
+	public int[] getLevel() {
+		return level;
 	}
-
-	public void run(JLabel timer) {
-		int i = 30;
-		while (i >= 0) {
-			System.out.println(timer.getBounds() + "    " + timer.getText());
-			timer.setText("" + i + "초");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			i--;
-		}
+	public void setLevel(int[] level) {
+		this.level = level;
 	}
+	public int getLev() {
+		return lev;
+	}
+	public void setLev(int lev) {
+		this.lev = lev;
+	}
+	
 }
 
-public class GameStart extends JFrame {
+class Cloud extends GameObject{
+	//x,y좌표
+	//속도
+	//터치메서드
+	//오염정도 (이미지로 바꿀 수 있게
+	private int x, y;
+	private int speed = 0; //y좌표에 추가해서 구름 내려가기
+	private int pollut =0;
+	String cimg[] = {"CloudClean0.png", "CloudClean1.png","CloudClean2.png","CloudClean3.png","CloudClean4.png"};
+	//구름오염도에 따른 구름!
+//	ImageIcon[] iic;	
+//	iic[0] = new ImageIcon("CloudClean0.png");
+//	iic1 = new ImageIcon("CloudClean1.png");
+//	iic2 = new ImageIcon("CloudClean2.png");
+//	iic3 = new ImageIcon("CloudClean3.png");
+//	iic4 = new ImageIcon("CloudClean4.png");
+//	
+	
+//	public void makeCloud() {
+//		pollut = (int)Math.random()*5;
+//		private JButton cloudButton = new JButton(new ImageIcon(cc[pollut]));
+//		x = (int)Math.random()*630;
+//		y = 0;
+//		speed = (int)Math.random()*20;
+//		//온클릭시 pollut--;
+//		//버튼 사이즈, add하기!;
+//	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getPollut() {
+		return pollut;
+	}
+
+	public void setPollut(int pollut) {
+		this.pollut = pollut;
+	}
+}
+public class GameStart extends JFrame{
+	GameObject go;
+	Cloud cloud[] = new Cloud[3];
 	Image screenImage;
 	Graphics screenGraphic;
 	private int mouseX, mouseY;
-	JPanel leftJP = new JPanel(); // 구름있는 패널
-	JPanel rightJP = new JPanel(); // 타임, 스코어, 네임, 메세지 패널
-
+	JPanel leftJP = new JPanel();     //구름있는 패널
+	JPanel rightJP = new JPanel();   //타임, 스코어, 네임, 메세지 패널
+	
+	
 	Image background = new ImageIcon(Main.class.getResource("../image/GameBG.png")).getImage();
-
+	
 	ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../image/exitButtonEntered.png"));
-	ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../image/exitButtonBasic.png"));
+	ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../image/exitButtonBasic.png")); 
 	JButton exitButton = new JButton(exitButtonBasicImage);
-
+	
 	JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../image/menuBar.png")));
-
-	// 구름오염도에 따른 구름!
-	ImageIcon[] str = new ImageIcon[5];
-	ImageIcon str0 = new ImageIcon("CloudClean0.png");
-	ImageIcon str1 = new ImageIcon("CloudClean1.png");
-	ImageIcon str2 = new ImageIcon("CloudClean2.png");
-	ImageIcon str3 = new ImageIcon("CloudClean3.png");
-	ImageIcon str4 = new ImageIcon("CloudClean4.png");
-	// 구름 ArrayList
-	ArrayList<Cloud> CloudList = new ArrayList<Cloud>();
 
 	// *****************************
 	JLabel title = new JLabel(new ImageIcon(Main.class.getResource("../image/titleImage.png")));
-	int CloudNum = 30; // 구름개수 1lev
+	
 	JLabel timer = new JLabel("100초");
-
+	
 	JLabel clock = new JLabel(new ImageIcon(Main.class.getResource("../image/Alarm01.png")));
-	JLabel money = new JLabel(new ImageIcon(Main.class.getResource("../image/money.png")));
-	JLabel mon = new JLabel("2500원");
-	JLabel smile = new JLabel(new ImageIcon(Main.class.getResource("../image/smile.png")));
-	JLabel ment = new JLabel(new ImageIcon(Main.class.getResource("../image/ment.png")));
 
-	public GameStart() {
-		setUndecorated(true); // 기본 메뉴바 안보임
+
+	
+	public GameStart(){
+		setUndecorated(true);  //기본 메뉴바 안보임
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true); // 화면에 보이게 해줌
-
-		// 사이즈
+		
+		//사이즈
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		leftJP.setSize(630, 600);
 		rightJP.setSize(265, 600);
-		// 배경
+		//배경
 		setBackground(new Color(0, 0, 0, 0));
 		leftJP.setBackground(new Color(0, 0, 0, 0));
 		rightJP.setBackground(new Color(0, 0, 0, 0));
-		// 레이아웃
-		setLayout(null);
-		leftJP.setLayout(null);
-		rightJP.setLayout(null);
-		// 위치
+		//레이아웃 
+		setLayout(null); 
+		leftJP.setLayout(null); 
+		rightJP.setLayout(null); 
+		//위치
 		leftJP.setBounds(25, 55, 640, 625);
 		rightJP.setBounds(690, 55, 285, 625);
-
-		setLocationRelativeTo(null); // 중앙배치
-
-		// 구름을 만들자!
-		for (int i = 0; i < CloudList.size(); i++) {
-			//Cloud cloud = CloudList.get(i);
-			CloudList.add(Cloud);
-		}
-		for (JButton cloud:CloudList) {
-			leftJP.add(cloud);
-		}
-		getContentPane().add(leftJP);
 		
-		dropClouds();
-
-		// *****************************
-
-		GameThread time = new GameThread();
-
-		title.setBounds(-5, 25, 300, 160);
-		rightJP.add(title);
-
-		clock.setBounds(55, 200, 55, 55);
-		rightJP.add(clock);
-
-		money.setBounds(55, 260, 55, 55);
-		rightJP.add(money);
-
-		// ment.setBounds(55, 400, 55, 55);
-		// rightJP.add(ment);
-		// smile.setBounds(55, 500, 55, 55);
-		// rightJP.add(smile);
-
-		timer.setFont(new Font("Gothic", Font.BOLD, 30)); // 레이블의 폰트 설정
-		timer.setBounds(125, 130, 200, 200);
-		rightJP.add(timer);
-
-		mon.setFont(new Font("Gothic", Font.BOLD, 30)); // 레이블의 폰트 설정
-		mon.setBounds(125, 190, 200, 200);
-		rightJP.add(mon);
-		// time.run(timer);
-
-		// 창끄기 버튼
-		exitButton.setBounds(965, 0, 30, 30);
+		setLocationRelativeTo(null); //중앙배치
+		
+		//창끄기 버튼
+		exitButton.setBounds(965,0,30, 30);
 		exitButton.setBorderPainted(false);
 		exitButton.setContentAreaFilled(false);
 		exitButton.setFocusPainted(false);
-		exitButton.addMouseListener(new MouseAdapter() {
-			public void mouseEntered(MouseEvent e) {
+		exitButton.addMouseListener(new MouseAdapter(){
+			public void mouseEntered(MouseEvent e){
 				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 				exitButton.setIcon(exitButtonEnteredImage);
 			}
-
-			public void mouseExited(MouseEvent e) {
+			public void mouseExited(MouseEvent e){
 				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 				exitButton.setIcon(exitButtonBasicImage);
 			}
-
-			public void mousePressed(MouseEvent e) {
+			public void mousePressed(MouseEvent e){
 				System.exit(0);
 			}
 		});
 		add(exitButton);
-
-		// 메뉴바
+		
+		//메뉴바
 		menuBar.setBounds(0, 0, 1000, 30);
-		menuBar.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
+		menuBar.addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e){
 				mouseX = e.getX();
 				mouseY = e.getY();
 			}
 		});
-		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
-			public void mouseDragged(MouseEvent e) {
-				int x = e.getXOnScreen(); // 현재스크린의 좌표
+		menuBar.addMouseMotionListener(new MouseMotionAdapter(){
+			public void mouseDragged(MouseEvent e){
+				int x = e.getXOnScreen();  //현재스크린의 좌표
 				int y = e.getYOnScreen();
-				setLocation(x - mouseX, y - mouseY);
+				setLocation(x-mouseX,y-mouseY);
 			}
 		});
 		add(menuBar);
+		
+		//색깔
+//		leftJP.setBackground(Color.BLUE);
+//		rightJP.setBackground(Color.RED);
+		JButton[] butCloud = new JButton[cloud.length];
 
+		for(int i=0; i<cloud.length; i++) {
+			cloud[i].setPollut((int)Math.random()*3+2);
+			butCloud[i] = new JButton(new ImageIcon(cloud[i].cimg[cloud[i].getPollut()]));
+			cloud[i].setX((int)Math.random()*630);
+			cloud[i].setY(0);
+			cloud[i].setSpeed((int)Math.random()*20);
+			//온클릭시 pollut--;
+			//버튼 사이즈, add하기!;
+			butCloud[i].setBounds(cloud[i].getX(), cloud[i].getY(), 160, 124);
+			leftJP.add(butCloud[i]);
+		}
+		//버튼 온클릭시 set바꿔주기
+		leftJP.setVisible(true);
+		add(leftJP);
+		rightJP.setVisible(true);
+		add(rightJP);
+		
 	}
-
+	
 	public void paint(Graphics g) {
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		screenGraphic = screenImage.getGraphics();
@@ -187,17 +217,10 @@ public class GameStart extends JFrame {
 	}
 
 	void screenDraw(Graphics g) {
-		g.drawImage(background, 0, 0, null); // 단순이미지(배경)
+		g.drawImage(background, 0, 0, null); //단순이미지(배경)
 		paintComponents(g); // 고정이미지(메뉴바)
 		this.repaint();
 	}
-
-	public void dropClouds() {
-		for (int i = 0; i < CloudNum; i++) {
-			CloudList.add(new Cloud(((int) (Math.random() * 655) + 25), 55, ((int) (Math.random() * 5)), 0));
-		}
-	}
-
 	public static void main(String[] args) {
 		new GameStart();
 	}
